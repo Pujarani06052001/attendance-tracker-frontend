964e0c10980f52ba614b0d6a225e02fc209ccd2c
Pujarani06052001 <ranipooja21@navgurukul.org>
2024-10-28 09:26:04 +0530

    Added navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import Navbar from "./components/Navbar.jsx";
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Signup from './components/Signup';
+import Login from './components/Login';
+
+function Home() {
+  return <h2>Home Page</h2>;
+}
+
 
 function App() {
   return (
-    <>
-      <h1>Attendance Tracker</h1>
+    <Router>
       <Navbar />
-    </>
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/Login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,25 @@
-const Navbar = () => {
-    return (
-      <h1>Navbar</h1>
-    );
-  };
-  
-  export default Navbar;
-  
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; 
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1 className="navbar-title">My Website</h1>
+      <ul className="navbar-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Signup">Signup Page</Link>
+        </li>
+        <li>
+          <Link to="/Login">Login</Link>
+        </li>
+       
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
